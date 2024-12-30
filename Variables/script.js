a2fbeb1025f73a7ac6e46753d47b700026a3b937
vivek <vivekdodiya1510@gmail.com>
2024-12-30 19:15:59 +0530

    commit

---
+++ Variables/script.js
@@ -0,0 +1,37 @@
+document.write("========== Variables ==========");
+
+document.write("<br>");
+document.write("Variables are used to store data in a program.");
+document.write("<br>");
+
+
+var a ;
+a = "=> This is a 'var' Variable"
+document.write(a);
+a = "=> we can redeclared and reassign value of 'var' variable"
+document.write("<br>");
+document.write(a);
+document.write("<br>");
+// var is global scope or function scope
+document.write("var is global scope or function scope");
+document.write("<br>");
+if(5===5){
+    a = 5
+}
+document.write(a + " this is num print from block");
+
+
+let b;
+b = "This is a 'let' Variable"
+document.write("<br>");
+document.write(b);
+document.write("<br>")
+b = "we can't redeclared but reassign value of 'let' variable"
+document.write(b);
+document.write("<br>");
+
+
+const c = "this is a 'const' variable"
+document.write("<br>");
+document.write(c);
+const d = "const is constunt variable we can't redeclared or reassign 'const' vatiable";
